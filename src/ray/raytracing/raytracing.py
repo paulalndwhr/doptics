b351a259d2b89546e2a7ac52297e32378bfa11e2
philipplndwhr <info@izwr.de>
2023-08-28 12:10:52 +0200

    intersection with $W$ reflector does not work yet

--- src/ray/raytracing/raytracing.py
+++ src/ray/raytracing/raytracing.py
@@ -3,7 +3,8 @@ from __future__ import division
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sp
-
+import matplotlib as mpl
+mpl.rcParams['figure.dpi'] = 100
 
 
 def line(p1, p2):
@@ -22,7 +23,7 @@ def intersection(l1, l2):
         y = dy / d
         return x, y
     else:
-        return False
+        return np.nan
 
 
 
@@ -48,7 +49,17 @@ pm2 = 1
 
 if __name__ == '__main__':
     # calculate theoretical target from the mapping between emitting density E and target density G1
-    m1_prime = lambda x, m: pm1 * E(x) / G1(m)
+    G1_int = sp.integrate.quad(G1, y1_span[0], y1_span[1])[0]
+    G1_fixed = lambda y1: G1(y1) / G1_int
+    E_int = sp.integrate.quad(E, x_span[0], x_span[1])[0]
+    E_fixed = lambda x: E(x) / E_int
+    # a2 = G1_int / sp.integrate.quad(G2, y2_span[0], y2_span[1])[0]
+    # print(f'calculated a2 as {a2}')
+    # G2_fixed = lambda y2: a2 * G2(y2)
+    # a1 = G1_int / sp.integrate.quad(E, x_span[0], x_span[1])[0]
+    # E_fixed = lambda x: a1 * E(x)
+
+    m1_prime = lambda x, m: pm1 * E_fixed(x) / G1_fixed(m)
     y1_0 = y1_span[1]
     m1_solved = sp.integrate.solve_ivp(m1_prime, x_span, [y1_0], dense_output=1)
     m1 = lambda x: m1_solved.sol(x)[0]
@@ -57,10 +68,26 @@ if __name__ == '__main__':
     reflectors = np.load("reflectors.npz")
     A = reflectors["arr_0"]  # A is discrete reflector as solved by ODE
     B = reflectors["arr_1"]  # B is ...
+    print(B)
     # List of (x, y) pairs
+    B_relevant = np.flipud(B)
+    print(B_relevant)
+    spline_A = sp.interpolate.CubicSpline(A[:, 0], A[:, 1])
+    spline_B = sp.interpolate.CubicSpline(B_relevant[:, 0], B_relevant[:, 1])
+    # spline_B = sp.interpolate.CubicSpline(np.flipud(B[:][0]), np.flipud(B[:][1]))
+    # spline_B = sp.interpolate.CubicSpline(np.flipud(B[:][0]), B[:][1])
 
-    errn = np.zeros(1000)
-    for n_rays in range(1, 1000):
+    # x_arr_u = np.linspace(x_span[0], x_span[1], 1000)
+    # x_arr_w = np.linspace(B[0][0], B[-1][0], 1000)
+    # plt.plot(x_arr_u, spline_A(x_arr_u), "r")
+    # plt.plot(A[:, 0], A[:, 1], "k")
+    # plt.plot(x_arr_w, spline_B(x_arr_w), "g")
+    # plt.plot(B[:, 0], B[:, 1], "k")
+    # plt.show()
+
+    RUNS = 30
+    errn = np.zeros(RUNS)
+    for n_rays in range(1, RUNS):
         # n_rays = 490
         print(n_rays)
         xis = np.linspace(0, 3, n_rays)
@@ -89,10 +116,20 @@ if __name__ == '__main__':
                     As[i] = o
                     Ps[i] = a
                     break
+            # find intersection with spline reflector
+            # implementation for parallel rays
+        hits_U = spline_A(xis)
+        hits_U_prime = spline_A(xis, 1)
+        U_reflection = - np.sin(np.arccos(hits_U_prime))
+        plt.plot(xis, hits_U)
+        plt.plot([xis, xis, xis + 10], [np.zeros(len(xis)), hits_U, hits_U + 10*U_reflection])
+        plt.show()
 
         # Second reflector
         Bs = np.zeros((n_rays, 2))
         u2s = np.zeros((n_rays, 2))
+
+        spline_intersections = np.zeros(len(xis))
         for i in range(n_rays):
             for j in range(B.shape[0] - 1):
                 a = As[i]
@@ -109,6 +146,19 @@ if __name__ == '__main__':
                     u2s[i] = e - 2 * np.dot(e, n) * n
                     Bs[i] = o
                     break
+            # calculate intersection with spline reflector
+            # ray_AB = lambda x: spline_A(xis[i]) - spline_A(xis[i], 1) * x
+            # diff = lambda x: spline_B(x) + ray_AB(x)
+            # spline_intersections[i] = sp.optimize.newton(diff, m1(xis[i]))  # , fprime2=lambda x: 6 * x)
+                # sp.optimize.bisect(diff, a=-20, b=20)
+        # spline_B
+        # hits_W = spline_A(xis)
+        # hits_W_prime = spline_A(xis, 1)
+        # W_reflection = np.sin(np.arccos(hits_U_prime))
+        # plt.plot(xis, hits_U)
+        # plt.plot([xis, xis, xis + 10], [np.zeros(len(xis)), hits_U, hits_U - 10 * U_reflection])
+        # plt.show()
+
 
         # Plotting
         # for i in range(n_rays):
@@ -136,13 +186,16 @@ if __name__ == '__main__':
                 error[i] = np.abs(targets[i] - intersec[0]) * E(xis[i])
                 # error[i] = (targets[i] - intersec[0]) * E(xis[i])
             except TypeError:
-                error[i] = -1
+                error[i] = np.nan
             # print(error)
         # error = error / n_rays
         # print(f'error: {error}\nsum(error): {sum(error)}')
-        error = error / n_rays
+        indices_to_drop = np.argwhere(error == np.nan)
+        error = np.delete(error, indices_to_drop)
+        error = error / len(error)
         # print(f'error: {error}\nsum(error): {sum(error)}')
         # print(f'targets: {targets}')
-        errn[n_rays] = sum(error)
-    plt.plot(np.arange(1, 1001, 1), errn)
+        errn[n_rays] = np.nansum(error)
+    plt.plot(np.arange(1, RUNS+1, 1), errn)
+    plt.title('ERTE, Affine Reflector')
     plt.show()
